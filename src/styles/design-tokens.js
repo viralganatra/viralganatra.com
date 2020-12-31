71b25b96aa9b1c54723c1ffa3010e63ebf1ebded
Viral Ganatra <viral@viralganatra.com>
2020-12-31 14:56:14 +0000

    Add design tokens to reference site variables

---
+++ src/styles/design-tokens.js
@@ -0,0 +1,66 @@
+import { px2rem } from './utils';
+
+/**
+ * Pixel equivalent: 8px
+ */
+export const TOKEN_SPACING_XS = px2rem(8);
+
+/**
+ * Pixel equivalent: 16px
+ */
+export const TOKEN_SPACING_SM = px2rem(16);
+
+/**
+ * Pixel equivalent: 24px
+ */
+export const TOKEN_SPACING_MD = px2rem(24);
+
+/**
+ * Pixel equivalent: 32px
+ */
+export const TOKEN_SPACING_LG = px2rem(32);
+
+/**
+ * Pixel equivalent: 48px
+ */
+export const TOKEN_SPACING_XL = px2rem(48);
+
+/**
+ * Pixel equivalent: 28px
+ */
+export const TOKEN_FONT_SIZE_ALPHA = px2rem(28);
+
+/**
+ * Pixel equivalent: 20px
+ */
+export const TOKEN_FONT_SIZE_BETA = px2rem(25);
+
+/**
+ * Pixel equivalent: 18px
+ */
+export const TOKEN_FONT_SIZE_GAMMA = px2rem(20);
+
+/**
+ * Pixel equivalent: 16px
+ */
+export const TOKEN_FONT_SIZE_BASE = px2rem(16);
+
+/**
+ * Pixel equivalent: 35px
+ */
+export const TOKEN_LINE_HEIGHT_ALPHA = px2rem(35);
+
+/**
+ * Pixel equivalent: 25px
+ */
+export const TOKEN_LINE_HEIGHT_BETA = px2rem(30);
+
+/**
+ * Pixel equivalent: 23px
+ */
+export const TOKEN_LINE_HEIGHT_GAMMA = px2rem(23);
+
+/**
+ * Pixel equivalent: 24px
+ */
+export const TOKEN_LINE_HEIGHT_BASE = px2rem(24);
