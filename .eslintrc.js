c4b95a5e1562416691d5ebdd342603eae57806e5
Viral Ganatra <viral@viralganatra.com>
2021-01-15 18:34:10 +0000

    Fix global variable so ESLint ignores it :bug:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   extends: [require.resolve('@viralganatra/app-scripts/configs/eslint')],
+  globals: {
+    graphql: true,
+    __PATH_PREFIX__: true,
+  },
   rules: {
     'react/react-in-jsx-scope': ['off'],
-    globals: {
-      __PATH_PREFIX__: true,
-    },
   },
 };
