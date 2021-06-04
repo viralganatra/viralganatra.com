3aa1b2ef7c5c1dfb1d3ba4b82b68c82f22d57998
Viral Ganatra <viral@viralganatra.com>
2021-06-04 17:07:05 +0100

    Standardise method arguments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
-const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
