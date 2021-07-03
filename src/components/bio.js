25d3a14ef93e79864420a6aba604ef59f8712be3
Viral Ganatra <viral@viralganatra.com>
2021-07-03 12:04:56 +0100

    Fix lint warnings :rotating_light:

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useStaticQuery, graphql, Link } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
 export default function Bio() {
   const data = useStaticQuery(graphql`
