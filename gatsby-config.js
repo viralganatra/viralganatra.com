84c52a5bdc577851d531220d72a74d0d749ef2d2
Viral Ganatra <viral@viralganatra.com>
2021-07-04 16:04:06 +0100

    Re-enable gatsby-plugin-offline :zap: (#1)
    * Reenable gatsby-plugin-offline
    
    * Fix PWA config :zap:

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,16 +95,9 @@ module.exports = {
       },
     },
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: 'gatsby-plugin-offline',
-    //   options: {
-    //     workboxConfig: {
-    //       globPatterns: ['**/icon-path*'],
-    //     },
-    //   },
-    // },
+    'gatsby-plugin-offline',
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
         trackingIds: [process.env.GA_API_TOKEN],
       },
